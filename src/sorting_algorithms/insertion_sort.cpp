f7971eab66cff7ab625a51b7c911f1fe062187b0
Chan <heochan01@korea.ac.kr>
2022-08-10 14:17:27 +0000

    Sorting Algorithms - Insertion Sort Code in C++

---
+++ src/sorting_algorithms/insertion_sort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+void insertion_sort() {
+    // 삽입 정렬 (Insertion Sort) 로 문제를 해결할 것이다.
+    // Big - O Notation : O(n^2)
+    // 문제 링크 : https://www.acmicpc.net/problem/2750
+
+
+    int N, a[1001];
+    int i, j, key;
+
+    cin >> N;
+    for(i=0; i<N; i++) {
+        cin>>a[i];
+    }    
+
+    for(i=1; i<N; i++) {
+        key = a[i];
+
+        for(j=i-1; j>=0; j--) {
+            if(key < a[j]) a[j+1] = a[j];
+            else break;
+        }
+        
+        a[j+1] = key;
+    }
+
+    for(i=0; i<N; i++) {
+        cout<<a[i]<<endl;
+    }
+}
