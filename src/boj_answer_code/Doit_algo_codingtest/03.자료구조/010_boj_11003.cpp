8df2f4a9960b20b77c1b5ec7593eb6d9d903dd56
Chan <heochan01@korea.ac.kr>
2023-02-25 14:41:56 +0000

    230225 - Do it! algo-codingtest 010 another answer code

--- src/boj_answer_code/Doit_algo_codingtest/03.자료구조/010_boj_11003.cpp
+++ src/boj_answer_code/Doit_algo_codingtest/03.자료구조/010_boj_11003.cpp
@@ -39,3 +39,48 @@ int main(void) {
 
     return 0;
 }
+
+
+// another answer code
+// 원래 AccessEmptyContainer 런타임 에러가 떴었으나 ... 해결하는 데 성공!
+// 디버깅의 중요성 ...
+#include <iostream>
+#include <deque>
+#include <vector>
+
+using namespace std;
+
+typedef pair<int, int> Node;
+
+int main1(void) { 
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, L;
+    cin>>N>>L;
+    
+    vector<int> A(N+1, 0);
+    for(int i=1; i<=N; i++) {
+        cin>>A[i];
+    }
+
+    deque<Node> dq;
+    dq.push_back(Node(1, A[1]));
+    cout<<dq.front().second<<" ";
+
+    for(int i=2; i<=N; i++) {
+        while(!dq.empty() && dq.back().second > A[i]) {
+            dq.pop_back();
+        }
+        dq.push_back(Node(i, A[i]));
+
+        if(i - dq.front().first >= L) dq.pop_front();
+
+        cout<<dq.front().second<<" "; 
+    }
+
+    cout << "\n";
+
+    return 0;
+}
