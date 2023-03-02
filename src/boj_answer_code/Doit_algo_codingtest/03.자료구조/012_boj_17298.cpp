10875aee5e711900f83d203067fa5fee693b46f7
Chan <heochan01@korea.ac.kr>
2023-03-02 11:49:37 +0000

    230302 - Do it! algo-codingtest 012 : Book Answer Code

--- src/boj_answer_code/Doit_algo_codingtest/03.자료구조/012_boj_17298.cpp
+++ src/boj_answer_code/Doit_algo_codingtest/03.자료구조/012_boj_17298.cpp
@@ -59,3 +59,45 @@ int main(void) {
     return 0;
 }
 
+
+// 책의 정답 코드(조금 다른 로직)
+int bookAns(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin>>N;
+    vector<int> A(N, 0);
+    vector<int> ans(N, 0);
+
+    for(int i=0; i<N; i++) {
+        cin>>A[i];
+    }
+
+    stack<int> myStack;
+    myStack.push(0);
+
+    for(int i=1; i<N; i++) {
+        // 스택이 비지 않고 현재 수열이 스택의 top 위치의 값보다 크다면
+        while(!myStack.empty() && A[myStack.top()] < A[i]) {
+            ans[myStack.top()] = A[i];  // 정답 배열에 오큰수를 현재 수열로 저장하기
+            myStack.pop();
+        }
+        myStack.push(i);    // 신규 데이터 push
+    }
+
+    while(!myStack.empty()) {
+        // 반복문을 다 돌고 나왔는데 스택에 남아 있는 값이 있다면 오큰수가 -1
+        ans[myStack.top()] = -1;
+        myStack.pop();
+    }
+
+    for(int i=0; i<N; i++) {
+        cout<<ans[i]<<"\n";
+    }
+
+    return 0;
+}
+
+
